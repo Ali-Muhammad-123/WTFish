72173a29f981aa9da479fbb34a9877ba3458be56
Ali-Muhammad-123 <Alim.9171@gmail.com>
2022-07-26 15:19:47 +0500

    added

--- src/components/footer.js
+++ src/components/footer.js
@@ -4,115 +4,319 @@ import appStore from "../images/app-store.png";
 import logo from "../images/logo.png";
 import soc1 from "../images/social-media1.png";
 import soc2 from "../images/social-media2.png";
+import { Link } from "react-scroll";
 function Footer(props) {
-	return (
-		<div className="footer-wrapper">
-			<div
-				style={{
-					display: "flex",
-					justifyContent: "space-between",
-				}}
-			>
-				<div>
-					<a
-						href="#"
-						style={{
-							textDecoration: "none",
-							color: "#111111",
-							fontSize: "18px",
-							fontWeight: "400",
-						}}
-					>
-						Home
-					</a>
-					<a
-						href="#"
-						style={{
-							textDecoration: "none",
-							color: "#111111",
-							fontSize: "18px",
-							fontWeight: "400",
-							padding: "0px 80px",
-						}}
-					>
-						Features
-					</a>
-					<a
-						href="#"
-						style={{
-							textDecoration: "none",
-							color: "#111111",
-							fontSize: "18px",
-							fontWeight: "400",
-						}}
-					>
-						FAQ
-					</a>
-				</div>
-				<div>
-					<button style={{ border: "none", backgroundColor: "transparent" }}>
-						<img src={appStore} style={{ width: "fit-content" }} />
-					</button>
-				</div>
-			</div>
-			<div style={{ display: "flex" }}>
-				<a href="#">
-					<img src={soc2} />
-				</a>
-				<a
-					href="#"
-					style={{
-						padding: "0px 24px",
-					}}
-				>
-					<img src={soc1} />
-				</a>
-			</div>
-			<div
-				style={{ display: "flex", padding: "38px 0px", alignItems: "center" }}
-			>
-				<img style={{ width: "100%", maxWidth: "100px" }} src={logo} />
-				<a
-					href="#"
-					style={{
-						textDecoration: "none",
-						color: "#111111",
-						fontSize: "14px",
-						fontWeight: "400",
-						paddingLeft: "40px",
-					}}
-				>
-					Conditions d’utilisation
-				</a>
-				<a
-					href="#"
-					style={{
-						textDecoration: "none",
-						color: "#111111",
-						fontSize: "14px",
-						fontWeight: "400",
-						padding: "0px 80px",
-					}}
-				>
-					RGPD
-				</a>
-				<a
-					href="#"
-					style={{
-						textDecoration: "none",
-						color: "#111111",
-						fontSize: "14px",
-						fontWeight: "400",
-					}}
-				>
-					Politique en matière de cookie
-				</a>
-			</div>
-			<div>
-				<p style={{ textAlign: "center" }}>WTFish copyright 2022</p>
-			</div>
-		</div>
-	);
+  return (
+    <div>
+      <div className="footer-wrapper">
+        <div
+          style={{
+            display: "flex",
+            justifyContent: "space-between",
+          }}
+        >
+          <div>
+            <a
+              href="#"
+              style={{
+                textDecoration: "none",
+                color: "#111111",
+                fontSize: "18px",
+                fontWeight: "400",
+              }}
+            >
+              <Link
+                activeClass="active"
+                className="test6"
+                to="home"
+                spy={true}
+                smooth={true}
+                duration={500}
+              >
+                Home
+              </Link>
+            </a>
+            <a
+              href="#"
+              style={{
+                textDecoration: "none",
+                color: "#111111",
+                fontSize: "18px",
+                fontWeight: "400",
+                padding: "0px 80px",
+              }}
+            >
+              <Link
+                activeClass="active"
+                className="test6"
+                to="features"
+                spy={true}
+                smooth={true}
+                duration={500}
+              >
+                Features
+              </Link>
+            </a>
+            <a
+              href="#"
+              style={{
+                textDecoration: "none",
+                color: "#111111",
+                fontSize: "18px",
+                fontWeight: "400",
+              }}
+            >
+              <Link
+                activeClass="active"
+                className="test6"
+                to="faqs"
+                spy={true}
+                smooth={true}
+                duration={500}
+              >
+                FAQ
+              </Link>
+            </a>
+          </div>
+          <div>
+            <button
+              style={{
+                border: "none",
+                backgroundColor: "transparent",
+                cursor: "pointer",
+              }}
+            >
+              <img src={appStore} style={{ width: "fit-content" }} />
+            </button>
+          </div>
+        </div>
+        <div style={{ display: "flex" }}>
+          <a href="#">
+            <img src={soc2} />
+          </a>
+          <a
+            href="#"
+            style={{
+              padding: "0px 24px",
+            }}
+          >
+            <img src={soc1} />
+          </a>
+        </div>
+        <div
+          style={{ display: "flex", padding: "38px 0px", alignItems: "center" }}
+        >
+          <img style={{ width: "100%", maxWidth: "100px" }} src={logo} />
+          <a
+            href="#"
+            style={{
+              textDecoration: "none",
+              color: "#111111",
+              fontSize: "14px",
+              fontWeight: "400",
+              paddingLeft: "40px",
+            }}
+          >
+            Conditions d’utilisation
+          </a>
+          <a
+            href="#"
+            style={{
+              textDecoration: "none",
+              color: "#111111",
+              fontSize: "14px",
+              fontWeight: "400",
+              padding: "0px 80px",
+            }}
+          >
+            RGPD
+          </a>
+          <a
+            href="#"
+            style={{
+              textDecoration: "none",
+              color: "#111111",
+              fontSize: "14px",
+              fontWeight: "400",
+            }}
+          >
+            Politique en matière de cookie
+          </a>
+        </div>
+        <div>
+          <p style={{ textAlign: "center" }}>WTFish copyright 2022</p>
+        </div>
+      </div>
+      <div className="footer-wrapper-mobile">
+        <div style={{ display: "flex", justifyContent: "center" }}>
+          <button style={{ border: "none", backgroundColor: "transparent" }}>
+            <img style={{ width: "100%", maxWidth: "100px" }} src={logo} />
+          </button>
+        </div>
+        <div
+          style={{
+            display: "flex",
+            justifyContent: "space-between",
+            padding: "20px 0px",
+          }}
+        >
+          <div
+            style={{
+              display: "flex",
+              justifyContent: "space-around",
+              width: "100%",
+            }}
+          >
+            <a
+              href="#"
+              style={{
+                textDecoration: "none",
+                color: "#111111",
+                fontSize: "18px",
+                fontWeight: "400",
+              }}
+            >
+              <Link
+                activeClass="active"
+                className="test6"
+                to="home"
+                spy={true}
+                smooth={true}
+                duration={500}
+              >
+                Home
+              </Link>
+            </a>
+            <a
+              href="#"
+              style={{
+                textDecoration: "none",
+                color: "#111111",
+                fontSize: "18px",
+                fontWeight: "400",
+              }}
+            >
+              <Link
+                activeClass="active"
+                className="test6"
+                to="features"
+                spy={true}
+                smooth={true}
+                duration={500}
+              >
+                Features
+              </Link>
+            </a>
+            <a
+              href="#"
+              style={{
+                textDecoration: "none",
+                color: "#111111",
+                fontSize: "18px",
+                fontWeight: "400",
+              }}
+            >
+              <Link
+                activeClass="active"
+                className="test6"
+                to="faqs"
+                spy={true}
+                smooth={true}
+                duration={500}
+              >
+                FAQ
+              </Link>
+            </a>
+          </div>
+        </div>
+
+        <div
+          style={{
+            display: "flex",
+            justifyContent: "center",
+            padding: "20px 0px",
+          }}
+        >
+          <button
+            style={{
+              border: "none",
+              backgroundColor: "transparent",
+              cursor: "pointer",
+            }}
+          >
+            <img src={appStore} style={{ width: "fit-content" }} />
+          </button>
+        </div>
+        <div style={{ display: "flex", justifyContent: "center" }}>
+          <a href="#">
+            <img src={soc2} />
+          </a>
+          <a
+            href="#"
+            style={{
+              padding: "0px 24px",
+            }}
+          >
+            <img src={soc1} />
+          </a>
+        </div>
+        <div
+          style={{
+            display: "flex",
+            padding: "38px 0px",
+            alignItems: "center",
+            flexDirection: "column",
+            justifyContent: "center",
+          }}
+        >
+          <a
+            href="#"
+            style={{
+              textDecoration: "none",
+              color: "#111111",
+              fontSize: "14px",
+              fontWeight: "400",
+              paddingLeft: "40px",
+              textAlign: "center",
+              padding: "10px 0px",
+            }}
+          >
+            Conditions d’utilisation
+          </a>
+          <a
+            href="#"
+            style={{
+              textDecoration: "none",
+              color: "#111111",
+              fontSize: "14px",
+              fontWeight: "400",
+              padding: "0px 80px",
+              padding: "10px 0px",
+            }}
+          >
+            RGPD
+          </a>
+          <a
+            href="#"
+            style={{
+              textDecoration: "none",
+              color: "#111111",
+              fontSize: "14px",
+              fontWeight: "400",
+              padding: "10px 0px",
+            }}
+          >
+            Politique en matière de cookie
+          </a>
+        </div>
+        <div>
+          <p style={{ textAlign: "center", paddingTop: "10px" }}>
+            WTFish copyright 2022
+          </p>
+        </div>
+      </div>
+    </div>
+  );
 }
 
 export default Footer;
--- src/components/navbar.js
+++ src/components/navbar.js
@@ -1,53 +1,193 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import "../styles/headerStyles.css";
 import logo from "../images/logo.png";
+import hamburger from "../images/hamburger.png";
+import { Link } from "react-scroll";
 function Navbar(props) {
-	return (
-		<div className="header-wrapper">
-			<div className="logo-div">
-				<img style={{ width: "100%", maxWidth: "100px" }} src={logo} />
-			</div>
-			<div>
-				<a
-					href="#"
-					style={{
-						textDecoration: "none",
-						color: "#111111",
-						fontSize: "18px",
-						fontWeight: "400",
-					}}
-				>
-					Home
-				</a>
-				<a
-					href="#"
-					style={{
-						textDecoration: "none",
-						color: "#111111",
-						fontSize: "18px",
-						fontWeight: "400",
-						padding: "0px 80px",
-					}}
-				>
-					Features
-				</a>
-				<a
-					href="#"
-					style={{
-						textDecoration: "none",
-						color: "#111111",
-						fontSize: "18px",
-						fontWeight: "400",
-					}}
-				>
-					FAQ
-				</a>
-			</div>
-			<div>
-				<button className="download-button">Download app</button>
-			</div>
-		</div>
-	);
+  const [windowSize, setWindowSize] = useState(getWindowSize());
+  const [mobileOpen, setMobileOpen] = useState(false);
+
+  useEffect(() => {
+    function handleWindowResize() {
+      setWindowSize(getWindowSize());
+    }
+
+    window.addEventListener("resize", handleWindowResize);
+
+    return () => {
+      window.removeEventListener("resize", handleWindowResize);
+    };
+  }, []);
+
+  function getWindowSize() {
+    const { innerWidth, innerHeight } = window;
+    return { innerWidth, innerHeight };
+  }
+  return (
+    <div className="header-wrapper">
+      <div
+        className="mobile-menu"
+        style={{
+          height: windowSize.innerHeight,
+          width: "100vw",
+          left: mobileOpen ? "0px" : "-100vw",
+        }}
+      >
+        <div
+          style={{
+            display: "flex",
+            flexDirection: "column",
+            padding: "50px 30px",
+          }}
+        >
+          <a
+            href="#"
+            style={{
+              textDecoration: "none",
+              color: "#111111",
+              fontSize: "40px",
+              fontWeight: "400",
+              padding: "50px 0px",
+            }}
+          >
+            <Link
+              activeClass="active"
+              className="test6"
+              to="home"
+              spy={true}
+              smooth={true}
+              duration={500}
+              onClick={() => setMobileOpen(false)}
+            >
+              Home
+            </Link>
+          </a>
+          <a
+            href="#"
+            style={{
+              textDecoration: "none",
+              color: "#111111",
+              fontSize: "40px",
+              fontWeight: "400",
+              padding: "50px 0px",
+            }}
+          >
+            <Link
+              activeClass="active"
+              className="test6"
+              to="features"
+              spy={true}
+              smooth={true}
+              duration={500}
+              onClick={() => setMobileOpen(false)}
+            >
+              Features
+            </Link>
+          </a>
+          <a
+            href="#"
+            style={{
+              textDecoration: "none",
+              color: "#111111",
+              fontSize: "40px",
+              fontWeight: "400",
+              padding: "50px 0px",
+            }}
+          >
+            <Link
+              activeClass="active"
+              className="test6"
+              to="faqs"
+              spy={true}
+              smooth={true}
+              duration={500}
+              onClick={() => setMobileOpen(false)}
+            >
+              FAQ
+            </Link>
+          </a>
+        </div>
+      </div>
+      <button
+        className="hamburger-button"
+        onClick={() => {
+          setMobileOpen(!mobileOpen);
+        }}
+      >
+        <img style={{ width: "100%", maxWidth: "20px" }} src={hamburger} />
+      </button>
+      <div className="logo-div">
+        <img style={{ width: "100%", maxWidth: "100px" }} src={logo} />
+      </div>
+      <div className="nav-links">
+        <a
+          href="#"
+          style={{
+            textDecoration: "none",
+            color: "#111111",
+            fontSize: "18px",
+            fontWeight: "400",
+          }}
+        >
+          {" "}
+          <Link
+            activeClass="active"
+            className="test6"
+            to="home"
+            spy={true}
+            smooth={true}
+            duration={500}
+          >
+            Home
+          </Link>
+        </a>
+        <a
+          href="#"
+          style={{
+            textDecoration: "none",
+            color: "#111111",
+            fontSize: "18px",
+            fontWeight: "400",
+            padding: "0px 80px",
+          }}
+        >
+          <Link
+            activeClass="active"
+            className="test6"
+            to="features"
+            spy={true}
+            smooth={true}
+            duration={500}
+          >
+            Features
+          </Link>
+        </a>
+        <a
+          href="#"
+          style={{
+            textDecoration: "none",
+            color: "#111111",
+            fontSize: "18px",
+            fontWeight: "400",
+          }}
+        >
+          <Link
+            activeClass="active"
+            className="test6"
+            to="faqs"
+            spy={true}
+            smooth={true}
+            duration={500}
+          >
+            FAQ
+          </Link>
+        </a>
+      </div>
+      <div>
+        <button className="download-button">Download app</button>
+      </div>
+    </div>
+  );
 }
 
 export default Navbar;
